290b4f00716d3db0db997e89baaaf7614e558ed3
Alex Sea <seagullalex81@gmail.com>
2024-06-10 17:55:13 +0300

    add task 1-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,19 @@
+function calcAverageCalories(days) {
+  let sumCalories = 0;
+  for (const day of days) {
+    // console.log(day);
+    if (typeof day.calories === "number") {
+      sumCalories += day.calories;
+      // console.log(sumCalories);
+    }
+  }
+  return sumCalories / 7;
+}
 
 
 
 
-
-console.log(alcAverageCalories([
+console.log(calcAverageCalories([
     { day: "monday", calories: 3010 },
     { day: "tuesday", calories: 3200 },
     { day: "wednesday", calories: 3120 },
@@ -13,6 +23,8 @@ console.log(alcAverageCalories([
     { day: "sunday", calories: 3300 }
   ])
 ); // 3180
+
+
 
 console.log(calcAverageCalories([
     { day: "monday", calories: 2040 },
@@ -28,4 +40,4 @@ console.log(calcAverageCalories([
 console.log(calcAverageCalories([])); // 0
 
 
-// Напиши функцію calcAverageCalories(days), яка повертає середньодобове значення кількості калорій, які спортсмен споживав протягом тижня. Функція очікує один параметр: days — масив об’єктів. Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день. Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
+// Напиши функцію calcAverageCalories(days), яка повертає середньодобове значення кількості калорій, які спортсмен споживав протягом тижня. Функція очікує один параметр: days — масив обєктів. Кожен обєкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день. Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,17 @@
-
-
 const profile = {
     username: "Jacob",
-  playTime: 300,
+    playTime: 300,
+    changeUsername(newName) {
+        this.username = newName
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
+    },
 };
+
 
 
 
@@ -21,11 +29,12 @@ console.log(profile.getInfo()); // "Marco has 320 active hours!"
 
 
 
-// Об’єкт profile описує профіль користувача на ігровій платформі. У його властивостях зберігається ім’я профілю username та кількість активних годин playTime, проведених у грі.
 
-// Доповни об’єкт profile методами для роботи з його властивостями.
+// Обєкт profile описує профіль користувача на ігровій платформі. У його властивостях зберігається імя профілю username та кількість активних годин playTime, проведених у грі.
 
-// Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
+// Доповни обєкт profile методами для роботи з його властивостями.
+
+// Метод changeUsername(newName) повинен приймати рядок (нове імя) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
 // Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
-// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це ім’я профілю, а <amount> — кількість ігрових годин.
+// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це імя профілю, а <amount> — кількість ігрових годин.
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
